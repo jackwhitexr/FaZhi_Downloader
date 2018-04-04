0da87cad2395787a33a80c19950f4a30f1ca6783
RuiRui <823528501@qq.com>
2018-04-04 20:22:44 +0800

    update old files

---
+++ Downloader.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Diagnostics;
+
+namespace FaZhi_Downloader
+{
+    /**
+     * 根据固定规则链接地址生成downloadLink.txt
+     * 调用aria2下载
+     * 重定向显示
+     */
+    class Downloader
+    {
+        private DateTime fromDate;
+        private DateTime toDate;
+        private string path;
+        private string defaultLink =
+            "http://live.njgb.com/NJRadio/FM106.9/*/fl3659/fl3659_$_143000-150000.mp3"; //固定下载链接
+        private  ArrayList dateRange = null;
+        private static string saveDir = System.Environment.CurrentDirectory + "\\log";
+        private static string stmpFile = saveDir + "\\temp.txt";
+        public Downloader(string path,DateTime fromDate, DateTime toDate)
+        {
+            this.path = path;
+            this.fromDate = fromDate;
+            this.toDate = toDate;
+        }
+        private void getDate()
+        {
+            dateRange = new ArrayList();
+            dateRange.Add(fromDate.ToString("yyyy-MM-dd")); //月份是大写的MM
+            DateTime tmpDate = fromDate;
+            while (tmpDate != toDate)
+            {
+                tmpDate = tmpDate.AddDays(1);
+                dateRange.Add(tmpDate.ToString("yyyy-MM-dd"));
+            }
+        }   
+        private void getDownloadLink(DateTime fromDate, DateTime toDate)
+        {
+            
+            if (File.Exists(stmpFile))
+            {
+                File.Delete(stmpFile);
+            }
+            if (Directory.Exists(saveDir))
+            {
+                Directory.Delete(saveDir);
+            }
+            Directory.CreateDirectory(saveDir);
+            //File.Create(stmpFile);
+            getDate();
+            using (StreamWriter sw = File.CreateText(stmpFile))
+            {
+                for (int i = 0; i < dateRange.Count; i++)
+                {
+                    string tmp = (string)dateRange[i]; tmp=tmp.Replace("/", "-");
+                    string tmp2 = tmp.Replace("-", "");
+                    string link1 =defaultLink.Replace("*",tmp);
+                    string link2 = link1.Replace("$", tmp2);
+                    sw.WriteLine(link2);
+                }
+            }
+
+        }
+        public void startDownload()
+        {
+            getDownloadLink(fromDate, toDate);
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            Process p = new Process();
+            p.StartInfo.FileName = System.Environment.CurrentDirectory + "\\aria2\\aria2c.exe";
+            p.StartInfo.Arguments = " -i " + stmpFile+"  -d "+path+"\\download";
+            p.Start();
+            p.WaitForExit();
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FaZhi_Downloader
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DateTime fromDate;
+        private DateTime toDate;
+        private bool confirmDate = false;
+        private string path = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            this.Closing += Window_Closing; //添加关闭窗口事件函数
+            CalendarDateRange cdrFrom = new CalendarDateRange(DateTime.Today.AddDays(1), DateTime.MaxValue);
+            this.fromDate_cal.BlackoutDates.Add(cdrFrom);
+            this.fromDate_cal.SelectedDate = DateTime.Today;
+            CalendarDateRange cdrTo = new CalendarDateRange(DateTime.Today.AddDays(1), DateTime.MaxValue);
+            this.toDate_cal.BlackoutDates.Add(cdrTo);
+            this.toDate_cal.SelectedDate = DateTime.Today;
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string saveDir = System.Environment.CurrentDirectory + "\\log";
+            string stmpFile = saveDir + "\\temp.txt";
+            if (File.Exists(stmpFile))
+            {
+                File.Delete(stmpFile);
+            }
+            if (Directory.Exists(saveDir))
+            { 
+                Directory.Delete(saveDir);
+            }
+        }
+        //选择保存路径按钮
+        private void choose_btn_Click(object sender, RoutedEventArgs e)
+        {
+            path = string.Empty;
+            var fbd = new FolderBrowserDialog(); //目录选择
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+            this.location_txt.Content = path;
+        }
+        //确定时间段
+        private void confirm_btn_Click(object sender, RoutedEventArgs e)
+        {
+            confirmDate = true;
+            fromDate = (DateTime)this.fromDate_cal.SelectedDate;
+            toDate = (DateTime)this.toDate_cal.SelectedDate;
+            {
+                this.chooseAns_txt.Content =
+                    "您选择的日期是：从" +
+                    fromDate.ToString("d") + "到" +
+                    toDate.ToString("d");
+            }
+
+        }
+        //下载确定按钮
+        private void downLoad_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!confirmDate)
+            {
+                System.Windows.MessageBox.Show("请先确定时间！");
+                return;
+            }
+            Downloader downloader = new Downloader(path,fromDate, toDate);
+            downloader.startDownload();
+        }
+
+
+
+    }
+}
